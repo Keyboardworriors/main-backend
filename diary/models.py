5b0bc0b177d4297f0196c5aec0838f6d7188a7fb
shinezeee <h46998600@gmail.com>
2025-03-14 14:27:26 +0900

    ✨ Diary,Music models 구현
    일기, 음악 저장 테이블 작성

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+# 음악정보 저장 테이블
+class Music(models.Model):
+    music_id = models.UUIDField(primary_key=True , unique=True)
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    thumbnail = models.CharField(max_length=255 , blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+# 다이어리 테이블
+class Diary(models.Model):
+    diary_id = models.UUIDField(primary_key=True , unique=True)
+    member_id = models.ForeignKey('member.Member', on_delete=models.CASCADE,related_name='diaries')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    rec_music = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
