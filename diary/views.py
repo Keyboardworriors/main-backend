a8155d231c1982fdf5cafce749a67fb62f3819e7
shinezeee <h46998600@gmail.com>
2025-03-17 10:21:23 +0900

    ✨ 일기조회 기능구현

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from diary.models import Diary
+from diary.serializers import DiarySerializer
+
+
+class DiaryAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, diary_id=None):
+        # 특정 일기 조회
+        if diary_id:
+            diary = get_object_or_404(Diary, id=diary_id, member=request.user)
+            serializer = DiarySerializer(diary)
+            return Response({
+                'message': 'Successfully retrieved diary.', # 일기 조회 성공 !
+                'data': serializer.data
+            },  status=status.HTTP_200_OK)
+
+        # 모든 일기 조회 ( 메인페이지 )
+        diaries = Diary.objects.filter(member=request.user)
+        serializer = DiarySerializer(diaries, many=True)
+        return Response({
+            'message': 'Successfully retrieved diaries.', # 모든 일기 불러오기 성공 !
+            'data': serializer.data
+        },  status=status.HTTP_200_OK)
