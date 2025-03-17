f17e38cdecd601d971c9d863b44bf161b673171f
shinezeee <h46998600@gmail.com>
2025-03-17 15:59:57 +0900

    🐛 run isort, black,

--- diary/urls.py
+++ diary/urls.py
@@ -5,8 +5,7 @@ from diary.views import DiaryAPIView
 app_name = "diary"
 urlpatterns = [
     path("api/diary", DiaryAPIView.as_view(), name="diary_main"),
-    path("api/diary/<diary_id:int>", DiaryAPIView.as_view(), name="diary_detail"),
+    path(
+        "api/diary/<diary_id:int>", DiaryAPIView.as_view(), name="diary_detail"
+    ),
 ]
-
-
-
--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,4 @@
 from django.utils import timezone
-
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
@@ -16,32 +15,43 @@ class DiaryAPIView(APIView):
     # 일기 조회
     def get(self, request, diary_id=None):
         # 요청 날짜 가져오기
-        date = request.query_params.get('date', None)
-        if date :
+        date = request.query_params.get("date", None)
+        if date:
             diary = Diary.objects.filter(
-                member = request.user.member,
+                member=request.user.member,
                 created_at__date=date,
             ).first()
-        # 특정 일기 조회
+            # 특정 일기 조회
             if diary_id:
                 serializer = DiarySerializer(diary_id)
-                return Response({
-                    'message': 'Successfully retrieved diary.', # 일기 조회 성공 !
-                    'data': serializer.data
-                },  status=status.HTTP_200_OK)
+                return Response(
+                    {
+                        "message": "일기 조회 성공.",
+                        "data": serializer.data,
+                    },
+                    status=status.HTTP_200_OK,
+                )
             # 일기가 없으면 일기 쓰기 안내
-            return Response({
-                'message': 'No diary found for the selected date.',
-                'data': None
-            }, status=status.HTTP_200_OK)
+            return Response(
+                {
+                    "message": "선택한 날짜에 일기가 없습니다.",
+                    "data": None,
+                },
+                status=status.HTTP_200_OK,
+            )
         # 모든 일기 조회 ( 메인페이지 ) -> 쓴 날짜 목록 보내기
-        diaries = Diary.objects.filter(member=request.user).values_list('created_at')
-        diary_dates = [diary['created_at'] .date() for diary in diaries]
+        diaries = Diary.objects.filter(member=request.user).values_list(
+            "created_at"
+        )
+        diary_dates = [diary["created_at"].date() for diary in diaries]
 
-        return Response({
-            'message': 'Successfully retrieved diary dates.',
-            'data': diary_dates,
-        },  status=status.HTTP_200_OK)
+        return Response(
+            {
+                "message": "일기 날짜데이터 불러오기 성공.",
+                "data": diary_dates,
+            },
+            status=status.HTTP_200_OK,
+        )
 
     # # 일기 작성
     # def post(self, request):
@@ -58,16 +68,23 @@ class DiaryAPIView(APIView):
     def delete(self, request, diary_id=None):
         diary = get_object_or_404(Diary, id=diary_id, member=request.user)
         if not diary_id:
-            return Response({
-                'error': 'invalid_request',
-                'message': '삭제할 diary_id가 필요합니다.'
-            }, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {
+                    "error": "invalid_request",
+                    "message": "삭제할 diary_id가 필요합니다.",
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         elif diary.member != request.user:
-            return Response({
-                "error": "forbidden",
-                "message": "권한이 없습니다. 본인의 일기만 삭제할 수 있습니다."
-            }, status=status.HTTP_403_FORBIDDEN)
+            return Response(
+                {
+                    "error": "forbidden",
+                    "message": "권한이 없습니다. 본인의 일기만 삭제할 수 있습니다.",
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
         diary.delete()
-        return Response({
-            'message': 'Successfully deleted diary.'
-        }, status=status.HTTP_200_OK)
+        return Response(
+            {"message": "일기가 성공적으로 삭제 되었습니다."},
+            status=status.HTTP_200_OK,
+        )
