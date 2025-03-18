2c45f2cda0d6b90141cbb7fedf2ceb5197606e18
shinezeee <h46998600@gmail.com>
2025-03-18 13:59:48 +0900

    ✨일기 작성 기능 구현

--- diary/urls.py
+++ diary/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from diary.views import DiaryAPIView
+from diary.views import DiarySearchAPIView, DiaryList, DiaryDetail, DiaryCreateAPIView, DiaryCustomDateCreateAPIView
 
 app_name = "diary"
 urlpatterns = [
-    path("api/diary", DiaryAPIView.as_view(), name="diary_main"),
-    path(
-        "api/diary/<diary_id:int>", DiaryAPIView.as_view(), name="diary_detail"
-    ),
+    path("api/diary/", DiaryList.as_view(), name="diary-main"),
+    path("api/diary/<int:diary_id>/", DiaryDetail.as_view(), name="diary-detail"),
     path(
         "api/diary/search/", DiarySearchAPIView.as_view(), name="diary-search"
     ),
+    path("api/diary/create/", DiaryCreateAPIView.as_view(), name="diary-create"),
+    path("api/diary/custom-date/",DiaryCustomDateCreateAPIView.as_view(), name="diary-custom-date"),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,6 @@
+import datetime
+
 from django.db.models import Q
-from django.utils import timezone
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
@@ -9,62 +10,45 @@ from rest_framework.views import APIView
 from diary.models import Diary
 from diary.serializers import DiarySerializer
 
-
-class DiaryAPIView(APIView):
+class DiaryList(APIView):
     permission_classes = [IsAuthenticated]
-
-    # 일기 조회
-    def get(self, request, diary_id=None):
-        # 요청 날짜 가져오기
-        date = request.query_params.get("date", None)
-        if date:
-            diary = Diary.objects.filter(
-                member=request.user.member,
-                created_at__date=date,
-            ).first()
-            # 특정 일기 조회
-            if diary_id:
-                serializer = DiarySerializer(diary_id)
-                return Response(
-                    {
-                        "message": "일기 조회 성공.",
-                        "data": serializer.data,
-                    },
-                    status=status.HTTP_200_OK,
-                )
-            # 일기가 없으면 일기 쓰기 안내
-            return Response(
-                {
-                    "message": "선택한 날짜에 일기가 없습니다.",
-                    "data": None,
-                },
-                status=status.HTTP_200_OK,
-            )
-        # 모든 일기 조회 ( 메인페이지 ) -> 쓴 날짜 목록 보내기
+    # 메인페이지에서의 일기 조회
+    def get(self, request):
+        # 일기 날짜 리스트만 조회
         diaries = Diary.objects.filter(member=request.user).values_list(
-            "created_at"
-        )
-        diary_dates = [diary["created_at"].date() for diary in diaries]
+            "created_at", flat=True)
+        diary_dates = [diary.date().strftime("%Y-%m-%d") for diary in diaries]
 
         return Response(
             {
-                "message": "일기 날짜데이터 불러오기 성공.",
+                "message": "일기 날짜 데이터 불러오기 성공.",
                 "data": diary_dates,
             },
             status=status.HTTP_200_OK,
         )
 
-    # # 일기 작성
-    # def post(self, request):
-    #     serializer = DiarySerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save(member=request.user, created_at=timezone.now())
-    #         return Response({
-    #             'message': 'Successfully created diary.', # 일기 작성 성공
-    #             'data': serializer.data
-    #         }, status=status.HTTP_201_CREATED
-    #             )
-
+class DiaryDetail(APIView):
+    permission_classes = [IsAuthenticated]
+    # 특정 일기 조회
+    def get(self, request, diary_id=None):
+        if diary_id:
+            diary = get_object_or_404(Diary, id=diary_id, member=request.user)
+            serializer = DiarySerializer(diary)
+            return Response(
+                {
+                    "message": "일기 조회 성공.",
+                    "data": serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        # 일기가 없으면 일기 쓰기 안내
+        return Response(
+            {
+                "message": "선택한 날짜에 일기가 없습니다.",
+                "data": None,
+            },
+            status=status.HTTP_200_OK,
+        )
     # 특정 일기 삭제
     def delete(self, request, diary_id=None):
         diary = get_object_or_404(Diary, id=diary_id, member=request.user)
@@ -80,16 +64,78 @@ class DiaryAPIView(APIView):
             return Response(
                 {
                     "error": "forbidden",
-                    "message": "권한이 없습니다. 본인의 일기만 삭제할 수 있습니다.",
+                    "message": "권한이 없습니다.",
                 },
                 status=status.HTTP_403_FORBIDDEN,
             )
         diary.delete()
         return Response(
-            {"message": "일기가 성공적으로 삭제 되었습니다."},
+            {"message": "Successfully deleted."},
             status=status.HTTP_200_OK,
         )
 
+class DiaryCreateAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    # 일기 작성
+    def post(self, request):
+        serializer = DiarySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(member=request.user)
+            return Response({
+                'message': 'Successfully created diary.', # 일기 작성 성공
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED
+                )
+
+class DiaryCustomDateCreateAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    # 작성일과 저장되는 일기날짜가 다를때
+    def post(self, request):
+        serializer = DiarySerializer(data=request.data)
+        if serializer.is_valid():
+            created_at = request.data.get('created_at', None)
+
+            # 날짜 없을때
+            if not created_at:
+                return Response({
+                    'error': "invalid_request",
+                'message': "작성날짜(created_at)를 입력해주세요."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            # 날짜형식 검증 (YYYY-MM-DD)
+            try:
+                created_date = datetime.datetime.strptime(created_at, "%Y-%m-%d").date()
+            except ValueError:
+                return Response({
+                    'error': "invalid_date_format",
+                    'meassage':"날짜 형식이 잘못되었습니다. (형식: YYYY-MM-DD)",
+                },status=status.HTTP_400_BAD_REQUEST)
+
+            # 미래 날짜 작성 불가
+            today = datetime.date.today()
+            if created_date > today:
+                return Response({
+                    'error':'future_date_not_allowed',
+                    'message':'미래의 날짜에는 일기를 작성할 수 없습니다.'
+                },status=status.HTTP_400_BAD_REQUEST)
+
+            # 하루에 한개 작성체크
+            if Diary.objects.filter(member=request.user, created_at__date=created_date).exists():
+                return Response({
+                    'error':'already_exists',
+                    'message':"해당 날짜에 이미 일기가 존재합니다."
+                })
+
+            # 저장
+            serializer.save(member=request.user, created_at=created_date)
+            return Response({
+                'message': 'Successfully created diary.',
+                'data': serializer.data
+            },status=status.HTTP_201_CREATED)
+        return Response({
+            'error': 'invalid_request',
+            'message':serializer.errors
+        },status=status.HTTP_400_BAD_REQUEST)
 
 # 일기 검색
 class DiarySearchAPIView(APIView):
@@ -111,7 +157,7 @@ class DiarySearchAPIView(APIView):
             )
         # 제목, 내용 에서 검색
         diaries = Diary.objects.filter(
-            Q(title__icontains=q) | Q(content__icontains=q), member=request.user
+            Q(diary_title__icontains=q) | Q(content__icontains=q), member=request.user
         ).order_by("-created_at")
 
         # 검색 데이터 없을 때
@@ -127,3 +173,5 @@ class DiarySearchAPIView(APIView):
             {"message": "일기 검색 성공", "data": serializer.data},
             status=status.HTTP_200_OK,
         )
+
+#
