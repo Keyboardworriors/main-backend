1a378794fa4ab4dd1b87c825d1a15b89811a81ba
shinezeee <h46998600@gmail.com>
2025-03-18 13:58:15 +0900

    🔧 다이어리테이블 title -> diary_title 로 변경

--- diary/models.py
+++ diary/models.py
@@ -18,7 +18,7 @@ class Diary(models.Model):
     member_id = models.ForeignKey(
         "member.Member", on_delete=models.CASCADE, related_name="diaries"
     )
-    title = models.CharField(max_length=100)
+    diary_title = models.CharField(max_length=100)
     content = models.TextField()
     moods = models.JSONField()
     rec_music = models.CharField(max_length=255, blank=True, null=True)
@@ -26,4 +26,4 @@ class Diary(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.diary_title
