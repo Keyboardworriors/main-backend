7f29ecdc617c9c83422bd6e6b425dfd70af610a7
shinezeee <h46998600@gmail.com>
2025-03-18 15:07:05 +0900

    🐛url 중복 수정

--- diary/urls.py
+++ diary/urls.py
@@ -10,18 +10,18 @@ from diary.views import (
 
 app_name = "diary"
 urlpatterns = [
-    path("api/diary/", DiaryList.as_view(), name="diary-main"),
+    path("", DiaryList.as_view(), name="diary-main"),
     path(
-        "api/diary/<int:diary_id>/", DiaryDetail.as_view(), name="diary-detail"
+        "<int:diary_id>/", DiaryDetail.as_view(), name="diary-detail"
     ),
     path(
-        "api/diary/search/", DiarySearchAPIView.as_view(), name="diary-search"
+        "search/", DiarySearchAPIView.as_view(), name="diary-search"
     ),
     path(
-        "api/diary/create/", DiaryCreateAPIView.as_view(), name="diary-create"
+        "create/", DiaryCreateAPIView.as_view(), name="diary-create"
     ),
     path(
-        "api/diary/custom-date/",
+        "custom-date/",
         DiaryCustomDateCreateAPIView.as_view(),
         name="diary-custom-date",
     ),
