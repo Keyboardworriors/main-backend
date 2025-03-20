7a9c626f94461c0d65b0cb415f051f3f8db012ae
rodzlen <qkqhrkxdkdy@naver.com>
2025-03-20 14:02:14 +0900

    :bug: 감정 추출 기능 api로 수정

--- diary/ai_urls.py
+++ diary/ai_urls.py
@@ -1,3 +1,9 @@
+from django.urls.conf import path
+
+from diary.views_ai import GetMoods
+
 app_name = "ai"
 
-urlpatterns = []
+urlpatterns = [
+    path("", GetMoods.as_view(), name="get_emotions"),
+]
