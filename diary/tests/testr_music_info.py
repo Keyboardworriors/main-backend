dc88d61f791846098fb471cebb8f268d312cccf5
shinezeee <h46998600@gmail.com>
2025-03-21 16:21:42 +0900

    ✅ music info test

---
+++ diary/tests/testr_music_info.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+
+from diary.views_music import get_youtube_info
+
+
+class TestMusicInfo(TestCase):
+
+    def test_music_info(self):
+        recommendations = [
+            {'title': '봄편지','artist': '아이유'},
+            {'title': 'like jennie','artist': 'jennie'},
+            {'title': 'MUTT', 'artist': 'leon thomas'},
+        ]
+        for rec in recommendations:
+            info = get_youtube_info(rec['title'], rec['artist'])
+            print(f"\n🎵 {rec['title']} - {rec['artist']} 검색 결과:")
+
+
+            self.assertIsNotNone(info, f"{rec['title']} 검색 실패 ❌")
+            self.assertIn("videoId", info)
+            self.assertIn("title", info)
+            self.assertIn("artist", info)
+            self.assertIn("thumbnail", info)
+            self.assertIn("embedUrl", info)
+            print(info)
+        print("✅ 모든 음악 정보 가져오기 테스트 통과!")
