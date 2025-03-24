25d2c49ef192851113d227c17ad3f763e6acb31b
shinezeee <h46998600@gmail.com>
2025-03-24 11:41:05 +0900

    🐛 url 오류 수정

--- config/urls/dev_urls.py
+++ config/urls/dev_urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/members/", include("member.urls.api_urls")),
     path("api/oauth/", include("member.urls.oauth_urls")),
-    path("api/diary/", include("diary.urls")),
+    path("api/diary/", include("diary.urls.diary_urls")),
     path("api/diary/recommendation-keyword", include("diary.urls.ai_urls")),
     path("api/diary/music/", include("diary.urls.music_urls")),
     path(
