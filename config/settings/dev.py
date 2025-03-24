f530adcd6b28ccab964e80ebffcea985f6567c89
rodzlen <qkqhrkxdkdy@naver.com>
2025-03-25 00:19:39 +0900

    :bug: change static url

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,10 +5,10 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ["*"]  # 개발 환경에서는 어디서든 접속 가능
 
-INSTALLED_APPS.append("debug_toolbar")  # 개발 도구 추가
+# INSTALLED_APPS.append("debug_toolbar")  # 개발 도구 추가
 
-MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
+# MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
 
-INTERNAL_IPS = ["127.0.0.1"]
+INTERNAL_IPS = ["*"]
 
 ROOT_URLCONF = "config.urls.dev_urls"
