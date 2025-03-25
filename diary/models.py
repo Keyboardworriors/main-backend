eedf8735ebb27c125a0d4155946911fb5c8ea155
shinezeee <h46998600@gmail.com>
2025-03-25 16:36:46 +0900

    🐛 음악저장필드 수정 , date필드 추가

--- diary/models.py
+++ diary/models.py
@@ -2,6 +2,8 @@ import uuid
 
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.db.models import JSONField
+from django.utils import timezone
 
 
 # 음악정보 저장 테이블
@@ -19,6 +21,13 @@ class Music(models.Model):
         return self.title
 
 
+from django.utils import timezone
+
+
+def get_today_date():
+    return timezone.now().date()
+
+
 # 다이어리 테이블
 class Diary(models.Model):
     diary_id = models.UUIDField(
@@ -30,7 +39,8 @@ class Diary(models.Model):
     diary_title = models.CharField(max_length=100)
     content = models.TextField()
     moods = ArrayField(models.CharField(max_length=20), default=list)
-    rec_music = models.CharField(max_length=255, blank=True, null=True)
+    rec_music = JSONField(blank=True, null=True)
+    date = models.DateField(default=get_today_date)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
