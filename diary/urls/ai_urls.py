9d6ef5e5afb5e5eb8e529d6f256556c3275153ab
shinezeee <h46998600@gmail.com>
2025-03-25 18:10:15 +0900

    🐛 오타 수정

--- diary/urls/ai_urls.py
+++ diary/urls/ai_urls.py
@@ -5,5 +5,5 @@ from diary.views.ai_views import GetMoods
 app_name = "ai"
 
 urlpatterns = [
-    path("/", GetMoods.as_view(), name="get_emotions"),
+    path("", GetMoods.as_view(), name="get_emotions"),
 ]
