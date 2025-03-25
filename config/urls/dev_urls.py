8decc678c93338b885a5ef23890ddb607a73d6ed
shinezeee <h46998600@gmail.com>
2025-03-25 18:10:15 +0900

    🐛 오타 수정

--- config/urls/dev_urls.py
+++ config/urls/dev_urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path("api/members/", include("member.urls.api_urls")),
     path("api/oauth/", include("member.urls.oauth_urls")),
     path("api/diary/", include("diary.urls.diary_urls")),
-    path("api/diary/recommendation-keyword", include("diary.urls.ai_urls")),
+    path("api/diary/recommendation-keyword/", include("diary.urls.ai_urls")),
     path("api/diary/music/", include("diary.urls.music_urls")),
     path(
         "swagger/",
